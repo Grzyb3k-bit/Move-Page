0c4b097f1096d125329cb9b93f1a27ff5947b6ca
Grzyb3k-bit <mikolajkolodziej859@gmail.com>
2025-07-22 15:53:05 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./pages/login.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Singup from "./pages/Signup.jsx";
+import AccontInfo from "./pages/account.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "login", element: <Login /> },
+  { path: "singup", element: <Singup /> },
+  { path: "account", element: <AccontInfo /> },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
